78d7f407cba923037505959c2e0670a148baea00
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-11-30 17:32:46 +0200

    deleted remove function from some table rows

--- components/TableOfData/TableOfData.tsx
+++ components/TableOfData/TableOfData.tsx
@@ -15,25 +15,19 @@ type UserTableItemProps = {
 export const TableOfData: React.FC<UserTableItemProps> = ({
   elementFromOurData,
 }) => {
-  const [showingEmail, setShowingEmail] = useState<boolean>(true);
-  const [showingCompany, setShowingCompany] = useState<boolean>(true);
-  const [showingAddress, setShowingAddress] = useState<boolean>(true);
-  const [showingNotes, setShowingNotes] = useState<boolean>(true);
 
   const rows = elementFromOurData.map((element) => (
     <Table.Tr key={element.id}>
       <Table.Td>{element.profile.first_name}</Table.Td>
       <Table.Td>{element.profile.last_name}</Table.Td>
-      {showingEmail && (
         <Table.Td>
           <a href={`mailto:${element.email}`}>{element.email}</a>
         </Table.Td>
-      )}
       <Table.Td>{element.profile.date_of_birth}</Table.Td>
-      {showingCompany && <Table.Td>{element.profile.company}</Table.Td>}
-      {showingAddress && <Table.Td>{element.profile.address}</Table.Td>}
+      <Table.Td>{element.profile.company}</Table.Td>
+      <Table.Td>{element.profile.address}</Table.Td>
       <Table.Td>{element.is_active ? "✅" : "❌"}</Table.Td>
-      {showingNotes && <Table.Td>{element.note}</Table.Td>}
+      <Table.Td>{element.note}</Table.Td>
     </Table.Tr>
   ));
 
@@ -57,14 +51,11 @@ export const TableOfData: React.FC<UserTableItemProps> = ({
             </Stack>
           </Table.Th>
 
-          {showingEmail && (
+          
             <Table.Th>
-              <Stack>
                 Email
-                <RemoveButton setState={setShowingEmail} />
-              </Stack>
             </Table.Th>
-          )}
+      
 
           <Table.Th>
             <Stack>
@@ -73,23 +64,17 @@ export const TableOfData: React.FC<UserTableItemProps> = ({
             </Stack>
           </Table.Th>
 
-          {showingCompany && (
-            <Table.Th>
-              <Stack>
-                Company
-                <RemoveButton setState={setShowingCompany} />
-              </Stack>
-            </Table.Th>
-          )}
 
-          {showingAddress && (
             <Table.Th>
-              <Stack>
-                Address
-                <RemoveButton setState={setShowingAddress} />
-              </Stack>
+              Company
             </Table.Th>
-          )}
+
+
+
+            <Table.Th>
+              Address
+            </Table.Th>
+
 
           <Table.Th>
             <Stack>
@@ -98,14 +83,11 @@ export const TableOfData: React.FC<UserTableItemProps> = ({
             </Stack>
           </Table.Th>
 
-          {showingNotes && (
+
             <Table.Th>
-              <Stack>
                 Notes
-                <RemoveButton setState={setShowingNotes} />
-              </Stack>
             </Table.Th>
-          )}
+
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>{rows}</Table.Tbody>
